5c07f45a110f553f5ae3260d4b897a9cb2df1193
BORBER <borber.drum@gmail.com>
2021-06-04 20:00:00 -0500

    [update] 添加 novel-gateway 网关

---
+++ novel-gateway/src/main/java/com/java2nb/novel/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.java2nb.novel.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ * @author Borber
+ * @version 1.0
+ * @since 2021/6/3
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
