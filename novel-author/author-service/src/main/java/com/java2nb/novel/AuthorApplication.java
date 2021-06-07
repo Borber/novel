9d7467239447a1893d36f8d30a331c32285c23ed
“LovingFore” <loving531@outlook.com>
2021-06-07 16:00:00 -0500

    [update] 添加 novel-author novel-book 基础模块

---
+++ novel-author/author-service/src/main/java/com/java2nb/novel/AuthorApplication.java
@@ -0,0 +1,24 @@
+package com.java2nb.novel;
+
+import com.java2nb.novel.common.cache.CacheService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ * 作家微服务启动器
+ * @author Loving
+ * @version 1.0
+ * @since 2021/6/07
+ */
+@SpringBootApplication
+//@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {CacheService.class})})
+@EnableFeignClients
+public class AuthorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorApplication.class);
+    }
+}
